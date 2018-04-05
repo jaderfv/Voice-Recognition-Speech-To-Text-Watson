3cdb07f5035145a15665a3aa1dc5c1b7c9f255b9
Jáder Friderichs <jaderfv@gmail.com>
2018-04-05 00:27:17 -0300

    Update Authentication.php

--- Authentication.php
+++ Authentication.php
@@ -2,8 +2,8 @@
 
 class Authentication{
 
-	public $user= "cc35a866-c7c7-421a-b0b5-a6f6b258ea0d";
-	public $password= "3VpaXMKRUiko";
+	public $user= "********-****-****-****-************";
+	public $password= "************";
 
 	public function getAuthentication(){
 		$userPassword= $this->user . ":" . $this->password;
